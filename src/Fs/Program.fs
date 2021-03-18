cc63ffe97e2ed8a5c1bec48d13be6436a0f53e3b
0918nobita <nobita.0918@gmail.com>
2021-03-18 13:24:30 +0900

    :sparkles: OS の識別処理を追加

--- src/Fs/Program.fs
+++ src/Fs/Program.fs
@@ -6,27 +6,39 @@ open System.Runtime.InteropServices
 [<Struct; IsReadOnly>]
 type Scene = Scene of id : int * title : string
 
-let inline selectRecords (conn : SqliteConnection) =
-    use cmd = conn.CreateCommand()
-    cmd.CommandText <- "SELECT * FROM scenes"
-    use dataReader = cmd.ExecuteReader()
-    seq {
-        while dataReader.Read() do
-            yield Scene(dataReader.GetInt32(0), dataReader.GetString(1))
-    }
-    |> List.ofSeq
-
 module Cpp =
     [<DllImport("bin/libgraphics.so")>]
     extern int run()
 
+type OSType = Windows64 | MacOS64 | Linux64 | Other
+
 [<EntryPoint>]
 let main _ =
-    printfn "isWindows: %b" <| OperatingSystem.IsWindows()
-    printfn "isMacOS: %b" <| OperatingSystem.IsMacOS()
-    printfn "isLinux: %b" <| OperatingSystem.IsLinux()
+    let inline getOSType () =
+        let is64bitOS = Environment.Is64BitOperatingSystem
+        let isWindows = OperatingSystem.IsWindows()
+        let isMacOS = OperatingSystem.IsMacOS()
+        let isLinux = OperatingSystem.IsLinux()
+        match (is64bitOS, isWindows, isMacOS, isLinux) with
+        | (true, true, false, false) -> Windows64
+        | (true, false, true, false) -> MacOS64
+        | (true, false, false, true) -> Linux64
+        | _ -> Other
+
+    printfn "OSType: %A" <| getOSType ()
 
     let task1 = async { Cpp.run() |> ignore }
+
+
+    let inline selectRecords (conn : SqliteConnection) =
+        use cmd = conn.CreateCommand()
+        cmd.CommandText <- "SELECT * FROM scenes"
+        use dataReader = cmd.ExecuteReader()
+        seq {
+            while dataReader.Read() do
+                yield Scene(dataReader.GetInt32(0), dataReader.GetString(1))
+        }
+        |> List.ofSeq
 
     let task2 =
         async {
