f072b8101a9d8214b82c271d1d340cae348ce160
0918nobita <nobita.0918@gmail.com>
2021-03-19 13:16:02 +0900

    :bug: macOS においてウィンドウが表示されない不具合を修正した
    GLFW を利用している C++ 関数がメインスレッド以外で呼び出されていることが原因でウィンドウが表示されなくなっていたので、メインスレッドで呼び出すようにした

--- src/fsharp/Program.fs
+++ src/fsharp/Program.fs
@@ -29,6 +29,26 @@ type Scene = Scene of id : int * title : string
 
 [<EntryPoint>]
 let main _ =
+    let inline selectRecords (conn : SqliteConnection) =
+        use cmd = conn.CreateCommand()
+        cmd.CommandText <- "SELECT * FROM scenes"
+        use dataReader = cmd.ExecuteReader()
+        seq {
+            while dataReader.Read() do
+                yield Scene(dataReader.GetInt32(0), dataReader.GetString(1))
+        }
+        |> List.ofSeq
+
+    let task =
+        async {
+            use conn = new SqliteConnection("Data Source=save_data.sqlite3")
+            conn.Open()
+            selectRecords conn
+            |> List.iter (printfn "%O")
+        }
+
+    Async.Start(task)
+
     let inline getOSType () =
         let is64bitOS = Environment.Is64BitOperatingSystem
         let isWindows = OperatingSystem.IsWindows()
@@ -40,37 +60,12 @@ let main _ =
         | (true, false, false, true) -> Linux64
         | _ -> Other
 
-    let task1 =
-        async {
-            let app =
-                match getOSType () with
-                | Windows64 -> failwith "Not implemented"
-                | MacOS64 -> VulkanAppForMacOS() :> VulkanApp
-                | Linux64 -> VulkanAppForLinux() :> VulkanApp
-                | Other -> failwith "This platform is not supported"
-            app.Run() |> ignore
-        }
+    let app =
+        match getOSType () with
+        | Windows64 -> failwith "Not implemented"
+        | MacOS64 -> VulkanAppForMacOS() :> VulkanApp
+        | Linux64 -> VulkanAppForLinux() :> VulkanApp
+        | Other -> failwith "This platform is not supported"
 
-    let inline selectRecords (conn : SqliteConnection) =
-        use cmd = conn.CreateCommand()
-        cmd.CommandText <- "SELECT * FROM scenes"
-        use dataReader = cmd.ExecuteReader()
-        seq {
-            while dataReader.Read() do
-                yield Scene(dataReader.GetInt32(0), dataReader.GetString(1))
-        }
-        |> List.ofSeq
-
-    let task2 =
-        async {
-            use conn = new SqliteConnection("Data Source=save_data.sqlite3")
-            conn.Open()
-            selectRecords conn
-            |> List.iter (printfn "%O")
-        }
-
-    [task1; task2]
-    |> Async.Parallel
-    |> Async.RunSynchronously
-    |> ignore
+    app.Run() |> ignore
     0
