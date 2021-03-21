70fea87e9e1f6fe52eee5a796cb80c8c94bd94ec
0918nobita <nobita.0918@gmail.com>
2021-03-21 16:41:04 +0900

    :sparkles: スクリプトエンジンでファイルの読み書きを実装した

--- src/DramaScript/Program.fs
+++ src/DramaScript/Program.fs
@@ -1,4 +1,18 @@
+open System.IO
+open System.Text
+
+let compileToBinaryFormat () =
+    let bytes = Encoding.ASCII.GetBytes "DRAMA"
+    File.WriteAllBytes("main.drama", bytes)
+
+let loadBinary () =
+    let bytes = File.ReadAllBytes("main.drama")
+    if bytes.Length < 5 || bytes.[0..5] <> [|68uy; 82uy; 65uy; 77uy; 65uy|]
+    then eprintfn "Failed to load binary: Invalid format"
+
 [<EntryPoint>]
-let main argv =
+let main _ =
     printfn "DramaScript Engine"
+    compileToBinaryFormat ()
+    loadBinary ()
     0
