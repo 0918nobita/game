e6c1f19395ce59a80ff7e8720524bd0dcad1438c
0918nobita <nobita.0918@gmail.com>
2021-03-27 01:04:01 +0900

    :sparkles: Scene 型を追加した

--- drama_script/GenScript.fsx
+++ drama_script/GenScript.fsx
@@ -1,5 +1,23 @@
 open System
 
+/// 状態遷移を表す型
+type State<'s, 't> = State of ('s -> 't * 's)
+
+module State =
+    /// 与えられた初期状態を遷移させ、結果と最終状態を返す
+    let inline run (initialState : 'a) (State(f) : State<'a, 'b>) : 'b * 'a =
+        f initialState
+
+    let inline bind<'s, 'a, 'b> (binder : 'a -> State<'s, 'b>) (state : State<'s, 'a>) : State<'s, 'b> =
+        State(fun s ->
+            let result, state' = run s state
+            binder result |> run state')
+
+type StateBuilder() =
+    member _.Return(v) = State(fun s -> (v, s))
+    member _.Bind(state, binder) =
+        State.bind binder state
+
 [<RequireQualifiedAccess>]
 type JsonValue =
     | String of string
@@ -68,19 +86,6 @@ type CharList =
                     []
                 |> JsonValue.Array
 
-(*
-module CharList =
-    let inline addNewChar (name : string) (charList : CharList) : CharId * CharList =
-        charList.AddChar(name)
-
-    let inline merge (CharList(latter)) (CharList(former)) : CharList =
-        latter
-        |> Map.fold
-            (fun acc charId charName -> Map.add charId charName acc)
-            former
-        |> CharList
-*)
-
 type Command =
     | Speak of speaker : CharId * message : string
 
@@ -107,54 +112,6 @@ type Script =
                         |> JsonValue.Object)
                 |> JsonValue.Array
 
-(*
-module Script =
-    let inline speak (charId : CharId) (message : string) (script : Script) : Script =
-        script.Speak(charId, message)
-
-    let inline append (Script(latter)) (Script(former)) =
-        Script(former @ latter)
-*)
-
-/// 状態遷移を表す型
-type State<'s, 't> = State of ('s -> 't * 's)
-
-module State =
-    /// 与えられた初期状態を遷移させ、結果と最終状態を返す
-    let inline run (initialState : 'a) (State(f) : State<'a, 'b>) : 'b * 'a =
-        f initialState
-
-    let inline bind<'s, 'a, 'b> (binder : 'a -> State<'s, 'b>) (state : State<'s, 'a>) : State<'s, 'b> =
-        State(fun s ->
-            let result, state' = run s state
-            binder result |> run state')
-
-    (*
-    /// 与えられた初期状態を遷移させ、結果を返す
-    let inline eval< ^a, ^b> (initialState : ^a) (State(f) : State< ^a, ^b>) : ^b =
-        fst <| f initialState
-
-    /// 与えられた初期状態を遷移させ、最終状態を返す
-    let inline exec< ^a, ^b> (initialState : ^a) (State(f) : State< ^a, ^b>) : ^a =
-        snd <| f initialState
-
-    /// 現在の状態を結果として受け取る State
-    let get = State(fun s -> (s, s))
-
-    /// 指定した値で状態を更新する
-    let inline put< ^a> (s : ^a) : State< ^a, unit> =
-        State(fun _ -> ((), s))
-
-    /// 指定した関数を適用して状態を更新する
-    let inline modify< ^a> (f : ^a -> ^a) : State< ^a, unit> =
-        State(fun s -> ((), f s))
-    *)
-
-type StateBuilder() =
-    member _.Return(v) = State(fun s -> (v, s))
-    member _.Bind(state, binder) =
-        State.bind binder state
-
 type ScriptBuilder() =
     member _.Yield(()) = Script []
 
@@ -162,15 +119,26 @@ type ScriptBuilder() =
     member _.Speak(script : Script, speaker : CharId, message : string) : Script =
         script.Speak(speaker, message)
 
+type Scene =
+    { charList : CharList
+      script : Script }
+
+    interface IJsonSerializable with
+        member this.ToJson() =
+            Map.empty
+                .Add("charList", JsonValue.from this.charList)
+                .Add("script", JsonValue.from this.script)
+            |> JsonValue.Object
+
 let () =
     let withChars = StateBuilder()
     let script = ScriptBuilder()
 
     let addChar name : State<CharList, CharId> =
-        State(fun (charList : CharList) -> charList.AddChar(name))
+        State(fun charList -> charList.AddChar(name))
 
     let speak (charId : CharId) (message : string) : State<Script, unit> =
-        State(fun (script : Script) -> ((), script.Speak(charId, message)))
+        State(fun script -> ((), script.Speak(charId, message)))
 
     let (script, charList) =
         withChars {
@@ -183,12 +151,7 @@ let () =
         }
         |> State.run CharList.Empty
 
-    charList
+    { charList = charList; script = script }
     |> JsonValue.from
     |> string
-    |> printfn "charList: %s"
-
-    script
-    |> JsonValue.from
-    |> string
-    |> printfn "script: %s"
+    |> printfn "%s"
