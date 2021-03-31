fe77cf46143f612d6ef4e36573ae02d37ffebb18
0918nobita <nobita.0918@gmail.com>
2021-03-31 21:20:09 +0900

    :recycle: [skip ci] TaskEither を用いてリファクタリングした

--- ds-compiler/src/index.ts
+++ ds-compiler/src/index.ts
@@ -1,21 +1,23 @@
-import * as O from 'fp-ts/lib/Option';
+import * as A from 'fp-ts/lib/Array';
+import * as TE from 'fp-ts/lib/TaskEither';
 import { pipe } from 'fp-ts/lib/pipeable';
 import { TextLintEngine } from 'textlint';
 
-const engine = new TextLintEngine({ rules: ['no-hankaku-kana'] });
-
-(async (): Promise<void> => {
-    const results = await engine.executeOnText('半角ｶﾀｶﾅ');
-
-    for (const result of results)
-        for (const msg of result.messages)
-            console.log(`(Ln ${msg.line}, Col ${msg.column}) ${msg.message}`);
-})();
-
 pipe(
-    O.bindTo('x')(O.some(10)),
-    O.map(({ x }) => x + 1),
-    O.chain(x => O.some(x + 2)),
-    O.getOrElse(() => 0),
-    x => console.log(x)
-);  // => 13
+    TE.tryCatch(
+        () => new TextLintEngine({ rules: ['no-hankaku-kana'] }).executeOnText('半角ｶﾀｶﾅ'),
+        (reason) => console.error(reason),
+    ),
+    TE.map(results =>
+        pipe(
+            results,
+            A.map(result => result.messages),
+            A.flatten,
+            A.reduce(
+                '',
+                (acc, msg) => `${acc}(Ln ${msg.line}, Col ${msg.column}) ${msg.message}\n`
+            ),
+            output => console.log(output)
+        )
+    ),
+)();
