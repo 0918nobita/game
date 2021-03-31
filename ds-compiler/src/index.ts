edac625169195092c9d0c05a38ada9b726ce3bf3
0918nobita <nobita.0918@gmail.com>
2021-04-01 01:33:29 +0900

    :sparkles: [skip ci] DramaScript コンパイラに校閲のロジックを追加した

--- ds-compiler/src/index.ts
+++ ds-compiler/src/index.ts
@@ -2,25 +2,55 @@ import * as A from 'fp-ts/lib/Array';
 import * as TE from 'fp-ts/lib/TaskEither';
 import { pipe } from 'fp-ts/lib/pipeable';
 import { TextLintEngine } from 'textlint';
+import { TextlintMessage } from '@textlint/types';
 
 const { version } = require('../package.json');
 console.log(`DramaScript Compiler v${version}`);
 
-pipe(
-    TE.tryCatch(
-        () => new TextLintEngine({ rules: ['no-hankaku-kana'] }).executeOnText('半角ｶﾀｶﾅ'),
-        (reason) => console.error(reason),
-    ),
-    TE.map(results =>
-        pipe(
-            results,
-            A.map(result => result.messages),
-            A.flatten,
-            A.reduce(
-                '',
-                (acc, msg) => `${acc}(Ln ${msg.line}, Col ${msg.column}) ${msg.message}\n`
-            ),
-            output => console.log(output)
+type LintTask = TE.TaskEither<string, TextlintMessage[]>;
+
+const runLintTask = (task: LintTask) => task();
+
+const lintText = (text: string): LintTask =>
+    pipe(
+        TE.tryCatch(
+            () => {
+                const rules =  [
+                    'max-ten',
+                    'no-doubled-conjunction',
+                    'no-doubled-conjunctive-particle-ga',
+                    'no-hankaku-kana',
+                ] as const;
+                const engine = new TextLintEngine({ rules });
+                return engine.executeOnText(text)
+            },
+            String
+        ),
+        TE.map(results =>
+            pipe(
+                results,
+                A.map(result => result.messages),
+                A.flatten
+            )
         )
-    ),
-)();
+    );
+
+const showLintMessages = (task: LintTask): LintTask =>
+    pipe(
+        task,
+        TE.map(messages => {
+            pipe(
+                messages,
+                A.map(msg =>
+                    console.log(`(Ln ${msg.line}, Col ${msg.column}) ${msg.message}`)
+                )
+            );
+            return messages;
+        })
+    );
+
+pipe(
+    lintText('しかし、私は以下のように思ったが、結果は予想どおりだったが、これは禁止されている半角ｶﾀｶﾅ。しかし、同じ接続詞が連続して出現している。'),
+    showLintMessages,
+    runLintTask
+);
