241f4950e7bfb20b8619e3f3d7eddb822e254ff5
0918nobita <nobita.0918@gmail.com>
2021-04-13 16:50:25 +0900

    :recycle: Do refactor

--- remake/src-tauri/src/main.rs
+++ remake/src-tauri/src/main.rs
@@ -6,19 +6,11 @@ fn main() {
     tauri::AppBuilder::new()
         .invoke_handler(|_webview, arg| {
             use cmd::Cmd::*;
-            match serde_json::from_str(arg) {
-                Err(e) => Err(e.to_string()),
-                Ok(command) => {
-                    match command {
-                        // definitions for your custom commands from Cmd here
-                        MyCustomCommand { argument } => {
-                            //  your command code
-                            println!("{}", argument);
-                        }
-                    }
-                    Ok(())
-                }
-            }
+            serde_json::from_str(arg)
+                .map(|command| match command {
+                    MyCustomCommand { argument } => println!("{}", argument),
+                })
+                .map_err(|e| e.to_string())
         })
         .build()
         .run();
