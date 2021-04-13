2e6f429ab211e34664bd61abc788c38ad31b3be2
0918nobita <nobita.0918@gmail.com>
2021-04-13 16:35:53 +0900

    :wrench: rustfmt の設定を修正し、コードをフォーマットした
    インデント幅を2から4に、1行の最大許容文字数を100文字から120文字に修正した

--- remake/src-tauri/src/main.rs
+++ remake/src-tauri/src/main.rs
@@ -1,30 +1,25 @@
-#![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
-)]
+#![cfg_attr(all(not(debug_assertions), target_os = "windows"), windows_subsystem = "windows")]
 
 mod cmd;
 
 fn main() {
-  tauri::AppBuilder::new()
-    .invoke_handler(|_webview, arg| {
-      use cmd::Cmd::*;
-      match serde_json::from_str(arg) {
-        Err(e) => {
-          Err(e.to_string())
-        }
-        Ok(command) => {
-          match command {
-            // definitions for your custom commands from Cmd here
-            MyCustomCommand { argument } => {
-              //  your command code
-              println!("{}", argument);
+    tauri::AppBuilder::new()
+        .invoke_handler(|_webview, arg| {
+            use cmd::Cmd::*;
+            match serde_json::from_str(arg) {
+                Err(e) => Err(e.to_string()),
+                Ok(command) => {
+                    match command {
+                        // definitions for your custom commands from Cmd here
+                        MyCustomCommand { argument } => {
+                            //  your command code
+                            println!("{}", argument);
+                        }
+                    }
+                    Ok(())
+                }
             }
-          }
-          Ok(())
-        }
-      }
-    })
-    .build()
-    .run();
+        })
+        .build()
+        .run();
 }
