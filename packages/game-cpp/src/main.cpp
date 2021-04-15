0027f007118be54c0274d720caafe1796c2bb375
0918nobita <nobita.0918@gmail.com>
2021-04-16 03:27:19 +0900

    :construction: C++ で空文字列を生成するサンプルを追加した

--- packages/game-cpp/src/main.cpp
+++ packages/game-cpp/src/main.cpp
@@ -3,12 +3,19 @@
 // #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
+#include <cassert>
 #include <iostream>
 #include <memory>
+#include <string>
 
 #include "window.hpp"
 
+// std::vector<const char *> getRequiredInstanceExtensions() noexcept;
+
 int main() {
+    std::string str; // 空文字列を生成する。
+    assert(str == "");
+
     std::unique_ptr<Window> window(new Window(600, 500, "Game"));
     (*window).eventLoop([](UNUSED GLFWwindow *window) {});
     return EXIT_SUCCESS;
