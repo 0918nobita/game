56eca45957a78233b9a5f424693b14ea96d39fb2
0918nobita <nobita.0918@gmail.com>
2021-04-16 14:40:31 +0900

    :hammer: build.ninja を修正した

--- packages/game-cpp/src/main.cpp
+++ packages/game-cpp/src/main.cpp
@@ -10,12 +10,7 @@
 
 #include "window.hpp"
 
-// std::vector<const char *> getRequiredInstanceExtensions() noexcept;
-
 int main() {
-    std::string str; // 空文字列を生成する。
-    assert(str == "");
-
     std::unique_ptr<Window> window(new Window(600, 500, "Game"));
     (*window).eventLoop([](UNUSED GLFWwindow *window) {});
     return EXIT_SUCCESS;
