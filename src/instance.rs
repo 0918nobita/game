e9b6f5ccac9e8cd9c6c90a66ba4422f09499eb76
0918nobita <nobita.0918@gmail.com>
2021-07-06 17:49:42 +0900

    :construction: GLFW が要求するインスタンス拡張を読み込み、ウィンドウサーフェスを生成するようにした

--- src/instance.rs
+++ src/instance.rs
@@ -1,16 +1,17 @@
 //! Vulkan インスタンス関連
 
-mod surface;
-
 use super::layer;
 use ash::{
     version::EntryV1_0,
     vk::{make_version, ApplicationInfo, InstanceCreateInfo},
     Entry, Instance,
 };
-use std::ffi::CString;
+use std::{ffi::CString, os::raw::c_char};
 
-pub fn create_instance(entry: &Entry) -> Result<Instance, Box<dyn std::error::Error>> {
+pub fn create_instance(
+    entry: &Entry,
+    extension_names: &Vec<String>,
+) -> Result<Instance, Box<dyn std::error::Error>> {
     let application_name = CString::new("Game")?;
     let application_name = &application_name.as_c_str();
 
@@ -24,8 +25,6 @@ pub fn create_instance(entry: &Entry) -> Result<Instance, Box<dyn std::error::Er
         .api_version(make_version(1, 0, 0))
         .build();
 
-    let extension_names = surface::get_surface_extensions();
-
     let layer_names = if cfg!(feature = "validation_layers") {
         debug!("Validation layers will be enabled");
         layer::check_validation_layer_support(&entry);
@@ -34,9 +33,16 @@ pub fn create_instance(entry: &Entry) -> Result<Instance, Box<dyn std::error::Er
         Vec::new()
     };
 
+    let extension_names: Vec<CString> = extension_names
+        .iter()
+        .map(|item| CString::new(item.as_str()).unwrap())
+        .collect();
+    let extension_names_raw: Vec<*const c_char> =
+        extension_names.iter().map(|item| item.as_ptr()).collect();
+
     let create_info = InstanceCreateInfo::builder()
         .application_info(&app_info)
-        .enabled_extension_names(&extension_names)
+        .enabled_extension_names(&extension_names_raw)
         .enabled_layer_names(&layer_names);
 
     Ok(unsafe { entry.create_instance(&create_info, None)? })
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use ash::{
     version::{DeviceV1_0, InstanceV1_0},
+    vk::Handle,
     Entry,
 };
-use glfw::Context;
 
 #[macro_use]
 extern crate log;
@@ -17,34 +17,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     trace!("Initialization started");
 
-    let entry = unsafe { Entry::new()? };
-
-    let instance = game::instance::create_instance(&entry)?;
-    defer! {
-        unsafe { instance.destroy_instance(None) }
-        trace!("Instance was released")
-    }
-
-    let physical_device = physical_device::pick_physical_device(&instance);
-
-    let (logical_device, _graphics_queue) =
-        logical_device::create_logical_device_with_graphics_queue(&instance, physical_device);
-    defer! {
-        unsafe { logical_device.destroy_device(None) }
-        trace!("Logical device was released");
-    }
-
-    trace!("Initialization completed");
-
     let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
-
-    let (mut window, event_receiver) = glfw
-        .create_window(500, 300, "Game", glfw::WindowMode::Windowed)
-        .expect("Failed to create window");
-
-    window.set_key_polling(true);
-    window.make_current();
-
+    glfw.window_hint(glfw::WindowHint::ClientApi(glfw::ClientApiHint::NoApi));
     assert!(glfw.vulkan_supported());
 
     let required_extensions = glfw.get_required_instance_extensions().unwrap();
@@ -53,6 +27,45 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         required_extensions
     );
 
+    let entry = unsafe { Entry::new()? };
+
+    let instance = game::instance::create_instance(&entry, &required_extensions)?;
+    defer! {
+        unsafe { instance.destroy_instance(None) }
+        trace!("Instance was destroyed")
+    }
+
+    let physical_device = physical_device::pick_physical_device(&instance);
+
+    let (logical_device, _graphics_queue) =
+        logical_device::create_logical_device_with_graphics_queue(&instance, physical_device);
+    defer! {
+        unsafe { logical_device.destroy_device(None) }
+        trace!("Logical device was destroyed");
+    }
+
+    let (mut window, event_receiver) = glfw
+        .create_window(500, 300, "Game", glfw::WindowMode::Windowed)
+        .expect("Failed to create window");
+
+    window.set_key_polling(true);
+
+    let mut raw_surface = 0;
+    let result = window.create_window_surface(
+        instance.handle().as_raw() as vk_sys::Instance,
+        std::ptr::null(),
+        &mut raw_surface,
+    );
+    assert!(result == 0, "Failed to create window surface");
+
+    let surface_loader = ash::extensions::khr::Surface::new(&entry, &instance);
+    let surface = ash::vk::SurfaceKHR::from_raw(raw_surface);
+    defer! {
+        unsafe { surface_loader.destroy_surface(surface, None) }
+        trace!("SurfaceKHR was destroyed")
+    }
+
+    trace!("Event loop started");
     while !window.should_close() {
         glfw.poll_events();
         for (_, event) in glfw::flush_messages(&event_receiver) {
@@ -63,5 +76,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             }
         }
     }
+
     Ok(())
 }
