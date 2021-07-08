89f8c8e5c7f58846567bcf17153c91e6945180e9
0918nobita <nobita.0918@gmail.com>
2021-07-08 23:26:31 +0900

    :construction: create_shader_module 関数を追加した

--- src/shader.rs
+++ src/shader.rs
@@ -1,4 +1,10 @@
-use ash::util::read_spv;
+use anyhow::Context;
+use ash::{
+    util::read_spv,
+    version::DeviceV1_0,
+    vk::{ShaderModule, ShaderModuleCreateInfo},
+    Device,
+};
 use once_cell::sync::Lazy;
 use std::io::Cursor;
 
@@ -11,3 +17,9 @@ pub static VERT_SHADER: Lazy<Vec<u32>> =
 
 pub static FRAG_SHADER: Lazy<Vec<u32>> =
     Lazy::new(|| read_spv(&mut Cursor::new(FRAG_SPV)).unwrap());
+
+pub fn create_shader_module(device: &Device, code: &[u32]) -> anyhow::Result<ShaderModule> {
+    let create_info = ShaderModuleCreateInfo::builder().code(code).build();
+    unsafe { device.create_shader_module(&create_info, None) }
+        .context("Failed to create shader module")
+}
