e26a066eae35e3f14c0b46bfbc018a36b51f397f
0918nobita <nobita.0918@gmail.com>
2021-07-10 16:16:37 +0900

    :fire: 全モジュールを削除

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,1 @@
 //! 開発中のインディーゲーム
-
-#[macro_use]
-extern crate log;
-#[macro_use]
-extern crate scopeguard;
-
-pub mod device;
-pub mod instance;
-mod layer;
-pub mod pipeline;
-pub mod shader;
-pub mod swapchain;
-pub mod window;
