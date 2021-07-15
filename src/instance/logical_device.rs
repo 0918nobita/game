db4b22e7d5403b637200a1a94b9d4598991f0773
0918nobita <nobita.0918@gmail.com>
2021-07-15 17:56:56 +0900

    :recycle: Do refactor

--- src/instance/logical_device.rs
+++ src/instance/logical_device.rs
@@ -1,15 +1,11 @@
 mod command_buffer;
 mod command_pool;
+mod image;
 
-use self::command_pool::ManagedCommandPool;
-use anyhow::Context;
+use self::{command_pool::ManagedCommandPool, image::ManagedImage};
 use ash::{
-    version::{DeviceV1_0, InstanceV1_0},
-    vk::{
-        Extent3D, Format, ImageCreateInfo, ImageLayout, ImageTiling, ImageType, ImageUsageFlags,
-        MemoryAllocateInfo, MemoryPropertyFlags, PhysicalDevice, Queue, SampleCountFlags,
-        SharingMode,
-    },
+    version::DeviceV1_0,
+    vk::{PhysicalDevice, Queue},
     Device, Instance,
 };
 
@@ -49,65 +45,14 @@ impl<'a> ManagedLogicalDevice<'a> {
         ManagedCommandPool::new(&self.device_raw, graphics_queue_family_index)
     }
 
-    pub fn create_image(&self, width: u32, height: u32) -> anyhow::Result<()> {
-        let create_info = ImageCreateInfo::builder()
-            .image_type(ImageType::TYPE_2D)
-            .extent(
-                Extent3D::builder()
-                    .width(width)
-                    .height(height)
-                    .depth(1)
-                    .build(),
-            )
-            .mip_levels(1)
-            .array_layers(1)
-            .format(Format::R8G8B8A8_UNORM)
-            .tiling(ImageTiling::OPTIMAL)
-            .initial_layout(ImageLayout::UNDEFINED)
-            .usage(ImageUsageFlags::COLOR_ATTACHMENT)
-            .sharing_mode(SharingMode::EXCLUSIVE)
-            .samples(SampleCountFlags::TYPE_1)
-            .build();
-        // 物理デバイスが持っているメモリについての情報
-        let memory_properties = unsafe {
-            self.instance_raw
-                .get_physical_device_memory_properties(self.physical_device)
-        };
-        let image = unsafe { self.device_raw.create_image(&create_info, None) }
-            .context("Failed to create image")?;
-        // イメージに対してどんな種類のメモリがどれくらいのサイズ必要か
-        let memory_requirements = unsafe { self.device_raw.get_image_memory_requirements(image) };
-        let memory_type_index = memory_properties
-            .memory_types
-            .iter()
-            .enumerate()
-            .find_map(|(index, memory_type)| {
-                let index = index as u32;
-                (memory_requirements.memory_type_bits & 2u32.pow(index) != 0
-                    && memory_type
-                        .property_flags
-                        .contains(MemoryPropertyFlags::DEVICE_LOCAL))
-                .then(|| index)
-            })
-            .context("No suitable memory type")?;
-        unsafe {
-            let memory = self
-                .device_raw
-                .allocate_memory(
-                    &MemoryAllocateInfo::builder()
-                        .allocation_size(memory_requirements.size)
-                        .memory_type_index(memory_type_index)
-                        .build(),
-                    None,
-                )
-                .context("Failed to allocate memory for image")?;
-            self.device_raw
-                .bind_image_memory(image, memory, 0)
-                .context("Failed to bind device memory to image")?;
-            self.device_raw.destroy_image(image, None);
-            self.device_raw.free_memory(memory, None);
-        }
-        Ok(())
+    pub fn create_image(&self, width: u32, height: u32) -> anyhow::Result<ManagedImage> {
+        ManagedImage::new(
+            self.instance_raw,
+            &self.physical_device,
+            &self.device_raw,
+            width,
+            height,
+        )
     }
 }
 
---
+++ src/instance/logical_device/image.rs
@@ -0,0 +1,93 @@
+use anyhow::Context;
+use ash::{
+    version::{DeviceV1_0, InstanceV1_0},
+    vk::{
+        DeviceMemory, Extent3D, Format, Image, ImageCreateInfo, ImageLayout, ImageTiling,
+        ImageType, ImageUsageFlags, MemoryAllocateInfo, MemoryPropertyFlags, PhysicalDevice,
+        SampleCountFlags, SharingMode,
+    },
+    Device, Instance,
+};
+
+pub struct ManagedImage<'a> {
+    device_raw: &'a Device,
+    device_memory_raw: DeviceMemory,
+    image_raw: Image,
+}
+
+impl<'a> ManagedImage<'a> {
+    pub fn new(
+        instance_raw: &Instance,
+        physical_device: &PhysicalDevice,
+        device_raw: &'a Device,
+        width: u32,
+        height: u32,
+    ) -> anyhow::Result<ManagedImage<'a>> {
+        let create_info = ImageCreateInfo::builder()
+            .image_type(ImageType::TYPE_2D)
+            .extent(
+                Extent3D::builder()
+                    .width(width)
+                    .height(height)
+                    .depth(1)
+                    .build(),
+            )
+            .mip_levels(1)
+            .array_layers(1)
+            .format(Format::R8G8B8A8_UNORM)
+            .tiling(ImageTiling::OPTIMAL)
+            .initial_layout(ImageLayout::UNDEFINED)
+            .usage(ImageUsageFlags::COLOR_ATTACHMENT)
+            .sharing_mode(SharingMode::EXCLUSIVE)
+            .samples(SampleCountFlags::TYPE_1)
+            .build();
+        // 物理デバイスが持っているメモリについての情報
+        let memory_properties =
+            unsafe { instance_raw.get_physical_device_memory_properties(*physical_device) };
+        let image_raw = unsafe { device_raw.create_image(&create_info, None) }
+            .context("Failed to create image")?;
+        // イメージに対してどんな種類のメモリがどれくらいのサイズ必要か
+        let memory_requirements = unsafe { device_raw.get_image_memory_requirements(image_raw) };
+        let memory_type_index = memory_properties
+            .memory_types
+            .iter()
+            .enumerate()
+            .find_map(|(index, memory_type)| {
+                let index = index as u32;
+                (memory_requirements.memory_type_bits & 2u32.pow(index) != 0
+                    && memory_type
+                        .property_flags
+                        .contains(MemoryPropertyFlags::DEVICE_LOCAL))
+                .then(|| index)
+            })
+            .context("No suitable memory type")?;
+        let device_memory_raw = unsafe {
+            device_raw.allocate_memory(
+                &MemoryAllocateInfo::builder()
+                    .allocation_size(memory_requirements.size)
+                    .memory_type_index(memory_type_index)
+                    .build(),
+                None,
+            )
+        }
+        .context("Failed to allocate memory for image")?;
+        unsafe { device_raw.bind_image_memory(image_raw, device_memory_raw, 0) }
+            .context("Failed to bind device memory to image")?;
+        Ok(ManagedImage {
+            device_raw,
+            device_memory_raw,
+            image_raw,
+        })
+    }
+}
+
+impl Drop for ManagedImage<'_> {
+    fn drop(&mut self) {
+        unsafe {
+            self.device_raw.destroy_image(self.image_raw, None);
+            trace!("Image was destroyed");
+            self.device_raw.free_memory(self.device_memory_raw, None);
+            trace!("GPU memory allocated for image was released");
+        }
+    }
+}
