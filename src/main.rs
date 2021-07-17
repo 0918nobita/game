d43bdcabd1a22a7ee8dd01d13073333e29e0fc57
0918nobita <nobita.0918@gmail.com>
2021-07-17 21:30:27 +0900

    :sparkles: Vulkan インスタンスを生成し物理デバイスを取得する処理を追加

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 extern crate game;
+#[macro_use]
+extern crate log;
 
 fn main() -> anyhow::Result<()> {
     env_logger::init();
+    let entry = unsafe { ash::Entry::new() }?;
+    let instance = game::Instance::new(&entry)?;
+    let physical_devices = instance.enumerate_physical_devices()?;
+    physical_devices
+        .iter()
+        .for_each(|physical_device| debug!("PhysicalDevice: {:?}", physical_device));
+    debug!("Complete!");
     Ok(())
 }
